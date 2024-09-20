32a291a5b1d7ad041eb6015a936958b412637614
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-21 02:50:44 +0300

    Added Car.List authorize

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -2,6 +2,7 @@
 using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,6 +21,7 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet("getall")]
+        [Authorize(Roles = "Car.List")]
         public IActionResult GetAll()
         {
           
